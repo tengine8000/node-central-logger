7bb76885e8de94130fdf3c628d0fbea6d6df01ce
tengine8000 <torty.emmanuel@gmail.com>
2025-09-21 16:48:52 +0100

    Add app secret and name to request headers

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,18 @@ api.get("/alive", (req, res) => {
   res.json("I am alive");
 });
 api.post("/log", async (req, res) => {
+  const secretKey = process.env.APP_SECRET;
+  const providedKey = req.headers["x-app-secret"];
+  const providedAppName = req.headers["x-app-name"];
+
+  if (providedKey !== secretKey || providedAppName !== process.env.APP_NAME) {
+    return res
+      .status(403)
+      .json({
+        status: "error",
+        message: "Forbidden: Invalid secret app name and key",
+      });
+  }
   try {
     await client.connect(); // Ensure connection
     const db = client.db("central_logger"); // Use your DB name
