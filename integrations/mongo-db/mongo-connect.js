e8c6da279e07827b85128cc2c32bd7b611556c80
tengine8000 <torty.emmanuel@gmail.com>
2025-09-21 17:07:38 +0100

    Remove insecure db logs

--- integrations/mongo-db/mongo-connect.js
+++ integrations/mongo-db/mongo-connect.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.MONGO_URI;
-console.log("MongoDB URI:", uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
